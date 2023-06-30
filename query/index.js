1a911935bdbe625a66c656da1f1d5bb08eff4133
faiz-webdev <faijuddin@tothenew.com>
2023-06-30 16:35:13 +0530

    13- updating comment

--- query/index.js
+++ query/index.js
@@ -27,7 +27,20 @@ app.post("/events", (req, res) => {
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
-console.log(posts)
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
+  console.log(posts);
   res.send({});
 });
 
