c134cede3b4f6eb118910aa8d99ee2065672be5d
faiz-webdev <faijuddin@tothenew.com>
2023-06-30 16:50:50 +0530

    14- rendering comments by status

--- moderation/index.js
+++ moderation/index.js
@@ -8,17 +8,21 @@ app.use(bodyParser.json());
 
 app.post("/events", async (req, res) => {
   const { type, data } = req.body;
-  const status = data.content.includes("orange") ? "rejected" : "approved";
 
-  await axios.post("http://localhost:4005/events", {
-    type: "CommentModerated",
-    data: {
-      id: data.id,
-      postId: data.postId,
-      status,
-      content: data.content,
-    },
-  });
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+
   res.send({});
 });
 
